3e9957c8daff5c704f7a9c57f515c8bb97addbea
Alexandra Kaudy <alexkaudy@gmail.com>
2021-03-29 21:09:52 -0600

    Change name in server_first_names

--- model-1.rb
+++ model-1.rb
@@ -8,6 +8,9 @@ open_wednesdays = false
 open_for_breakfast = true
 has_kids_menu = true
 is_wheelchair_accessible = true
-server_first_names = ["Julie", "John", "Matthew", "Christopher", "Cindy", "Alexandra"]
+server_first_names = ["Julie", "Kyle", "Matthew", "Christopher", "Cindy", "Alexandra"]
 cook_first_names = ["Jake", "Max", "Jennifer"]
 open_days = ["Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"]
+
+# Add variables assigned to hashes.
+# Look up hashes and consult slack.
