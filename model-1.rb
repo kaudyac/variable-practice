aaf6e9332d71881d293354df211d30afd87170ec
Alexandra Kaudy <alexkaudy@gmail.com>
2021-03-29 20:53:45 -0600

    Add variables to model-1.rb

--- model-1.rb
+++ model-1.rb
@@ -1,3 +1,6 @@
 restaurant_name = "EG's Garden Grill"
 happy_hour_special = "Buy One Get One Free Appetizers"
 ladies_night_special = "Ladies Well Drinks $1 Each"
+total_number_servers = 9
+total_number_cooks = 5
+cost_beer_order = 872.63
