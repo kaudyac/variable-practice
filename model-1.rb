1bab4dc0b75ddccba6e1db09273aad67d64d8b9e
Alexandra Kaudy <alexkaudy@gmail.com>
2021-04-01 16:13:13 -0600

    Edit hashes in model-1.rb

--- model-1.rb
+++ model-1.rb
@@ -15,16 +15,19 @@ open_days = ["Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"]
 # Add variables assigned to hashes.
 # Look up hashes and consult slack.
 
-server_hourly_wage = Hash.new
-server_hourly_wage ["Kyle"] = 7.25
-server_hourly_wage ["Julie"] = 7.25
-server_hourly_wage ["Alexandra"] = 7.25
-server_hourly_wage ["Matthew"] = 7.25
-server_hourly_wage ["Cindy"] = 7.25
-server_hourly_wage ["Christopher"] = 8.00
+server_hourly_wage = {
+server_hourly_wage ["Kyle"] => 7.25
+server_hourly_wage ["Julie"] => 7.25
+server_hourly_wage ["Alexandra"] => 7.25
+server_hourly_wage ["Matthew"] => 7.25
+server_hourly_wage ["Cindy"] => 7.25
+server_hourly_wage ["Christopher"] => 8.00
+}
 
-#There are a LOT of ways to denote hashes!
-
+# Add hashes defining side options for specific meals.
 side_options = {:dinner => "Cup of Soup", "Side Salad", "Baked Potato" }
 side_options = {:lunch => "Zucchini Fries", "Cup of Soup", "Side Salad"}
 side_options = {:breakfast => "Seasonal Fruit", "Roasted Potatoes", "Sourdough Toast and Jam"}
+
+
+#Can an instructor please look at this and comment?
